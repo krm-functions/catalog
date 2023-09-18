2c25b4a382aae1510a6b8a3d55833be39efdcd70
michaelvl <mvl.gh@network42.dk>
2023-09-18 20:49:47 +0200

    Add template function

---
+++ cmd/template/main.go
@@ -0,0 +1,46 @@
+// Copyright 2023 Michael Vittrup Larsen
+
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+
+//     http://www.apache.org/licenses/LICENSE-2.0
+
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package main
+
+import (
+	"context"
+	"fmt"
+	"os"
+
+	"github.com/GoogleContainerTools/kpt-functions-sdk/go/fn"
+)
+
+var _ fn.Runner = &YourFunction{}
+
+type YourFunction struct {
+	FnConfigBool bool
+	FnConfigInt  int
+	FnConfigFoo  string
+}
+
+func (r *YourFunction) Run(_ *fn.Context, functionConfig *fn.KubeObject, items fn.KubeObjects, results *fn.Results) bool {
+	*results = append(*results, fn.GeneralResult(fmt.Sprintf("functionConfig (%T): %+v\n", functionConfig, functionConfig), fn.Info))
+	for _, ko := range items {
+		*results = append(*results, fn.GeneralResult(fmt.Sprintf("Obj : %+v", ko.GetId()), fn.Info))
+	}
+	return true
+}
+
+func main() {
+	runner := fn.WithContext(context.Background(), &YourFunction{})
+	if err := fn.AsMain(runner); err != nil {
+		os.Exit(1)
+	}
+}
