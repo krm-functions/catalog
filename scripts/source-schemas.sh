fb64097b027f4661632c0f56238ca06bf545b1b5
michaelvl <mvl.gh@network42.dk>
2024-07-17 20:18:25 +0200

    Fix datree crd branch name

--- scripts/source-schemas.sh
+++ scripts/source-schemas.sh
@@ -6,7 +6,7 @@ TMP_SCHEMAS=tmp-schemas
 mkdir -p "$TMP_SCHEMAS/kubernetes-json-schema" "$TMP_SCHEMAS/CRDs-catalog"
 
 git clone --depth 1 --single-branch --branch master https://github.com/yannh/kubernetes-json-schema.git "$TMP_SCHEMAS/kubernetes-json-schema"
-git clone --depth 1 --single-branch --branch master https://github.com/datreeio/CRDs-catalog.git        "$TMP_SCHEMAS/CRDs-catalog"
+git clone --depth 1 --single-branch --branch main   https://github.com/datreeio/CRDs-catalog.git        "$TMP_SCHEMAS/CRDs-catalog"
 
 # Build schema bundle
 SCHEMAS_BUNDLE=schema-bundle
