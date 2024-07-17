09a9b31fe8f33b905806bb266e07a42b80a075a5
michaelvl <mvl.gh@network42.dk>
2024-07-17 16:05:45 +0200

    Improve kubeconform schema handling and generation

--- scripts/source-schemas.sh
+++ scripts/source-schemas.sh
@@ -5,11 +5,13 @@ set -ex
 TMP_SCHEMAS=tmp-schemas
 mkdir -p "$TMP_SCHEMAS/kubernetes-json-schema" "$TMP_SCHEMAS/CRDs-catalog"
 
-git clone --depth 1 https://github.com/yannh/kubernetes-json-schema.git "$TMP_SCHEMAS/kubernetes-json-schema"
-git clone --depth 1 https://github.com/datreeio/CRDs-catalog.git        "$TMP_SCHEMAS/CRDs-catalog"
+git clone --depth 1 --single-branch --branch master https://github.com/yannh/kubernetes-json-schema.git "$TMP_SCHEMAS/kubernetes-json-schema"
+git clone --depth 1 --single-branch --branch master https://github.com/datreeio/CRDs-catalog.git        "$TMP_SCHEMAS/CRDs-catalog"
 
 # Build schema bundle
 SCHEMAS_BUNDLE=schema-bundle
 mkdir -p "$SCHEMAS_BUNDLE"
 cp -r "$TMP_SCHEMAS/kubernetes-json-schema/master-standalone-strict" "$SCHEMAS_BUNDLE/"
 cp -r "$TMP_SCHEMAS/CRDs-catalog" "$SCHEMAS_BUNDLE/"
+
+rm -rf "$TMP_SCHEMAS"
